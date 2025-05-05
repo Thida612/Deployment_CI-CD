0cf360e3ceb87ce197d68d60524282b80ae6ae1f
Thida612 <angelic722@gmail.com>
2025-05-05 06:52:51 -0500

    fix syntax error in the cleanDB.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -13,8 +13,14 @@ export default defineConfig({
 
   e2e: {
     baseUrl: 'http://localhost:3001',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    setupNodeEvents(on) {
+      // Example usage of 'on' to avoid the error
+      on('task', {
+        log(message) {
+          console.log(message);
+          return null;
+        },
+      });
     },
   },
 });
